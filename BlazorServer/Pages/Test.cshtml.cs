8cc459abda2d5e6d20cfbfb9e63667bbe7484c75
Iggy988 <igor.rodic1988@gmail.com>
2023-11-24 22:04:15 +0100

    Add project files.

---
+++ BlazorServer/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorServer.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
